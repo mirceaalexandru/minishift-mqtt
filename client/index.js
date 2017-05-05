8be3bde44cd192fcf0747960588748933c76fb2a
malex <mircea.alexandru@gmail.com>
2017-05-05 09:15:19 +0300

    first draft

---
+++ client/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mqtt = require('mqtt');
+const restify = require('restify');
+const stringify = require('fast-safe-stringify');
+
+const mqttBroker = `mqtt://${process.env.MQTT_BROKER}`;
+
+console.log(`Connecting to MQTT broker ${mqttBroker}`);
+const client  = mqtt.connect(mqttBroker);
+
+function ping(req, res, next) {
+	console.log('ping');
+	const message = {
+		code: 'pong',
+		timestamp: new Date()
+	};
+
+	client.publish('ping', `Sent message: ${stringify(message)}`);
+
+	res.json(message);
+	next();
+}
+
+function start() {
+	const port = process.env.MOCK_SERVER_PORT || 3050;
+	console.log(`Listen on ${port}`);
+	server.listen(port);
+}
+
+var server = restify.createServer({
+	name: 'Mock Server'
+});
+
+server.get('/ping', ping);
+client.on('connect', start);
