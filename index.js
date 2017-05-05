013276141be614f806053f06a87f1ec2f52374ad
malex <mircea.alexandru@gmail.com>
2017-05-05 09:28:29 +0300

    changes

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 'use strict';
 
-const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://test.mosquitto.org')
+const mqtt = require('mqtt');
+const restify = require('restify');
+const stringify = require('fast-safe-stringify');
 
-client.on('connect', function () {
-	client.subscribe('ping')
-})
+const mqttBroker = `mqtt://${process.env.MQTT_BROKER}`;
 
-client.on('message', function (topic, message) {
-	// message is Buffer
-	console.log(message.toString());
-})
+console.log(`Connecting to MQTT broker ${mqttBroker}`);
+const client  = mqtt.connect(mqttBroker);
+
+function ping(req, res, next) {
+	console.log('ping');
+	const message = {
+		code: 'pong',
+		timestamp: new Date()
+	};
+
+	client.publish('ping', `Sent message: ${stringify(message)}`);
+
+	res.json(message);
+	next();
+}
+
+function start() {
+	console.log(`Connected to MQTT broker...`);
+	const port = process.env.MOCK_SERVER_PORT || 3050;
+	console.log(`Listen on ${port}`);
+	server.listen(port);
+}
+
+var server = restify.createServer({
+	name: 'Mock Server'
+});
+
+server.get('/ping', ping);
+client.on('connect', start);
